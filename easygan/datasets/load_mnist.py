25a277d7eb11d834a7935d8810910272fe592438
Prem-kumar27 <sprem1997@gmail.com>
2020-03-02 00:57:03 +0530

    Added CGan

--- easygan/datasets/load_mnist.py
+++ easygan/datasets/load_mnist.py
@@ -16,3 +16,12 @@ def load_mnist():
     train_data = train_data.reshape((-1, 28, 28, 1))
 
     return train_data
+
+def load_mnist_with_labels():
+	(x_train, train_labels), (x_test, test_labels) = tf.keras.datasets.mnist.load_data()
+	train_data = np.concatenate((x_train, x_test), 0)
+	train_labels = np.concatenate((train_labels, test_labels), 0)
+	train_data = train_data.astype('float32')
+	train_data = train_data.reshape((-1, 28, 28, 1))
+	train_labels = train_labels.reshape((-1, 1))
+	return train_data,train_labels
